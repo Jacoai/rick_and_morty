e2981ce28ac45d936993263b406c115b29fe6c37
Jacoai <grotesquevodevil@gmail.com>
2025-05-22 19:43:39 +0300

    remove unised

--- lib/core/injector/injector.dart
+++ lib/core/injector/injector.dart
@@ -1,10 +1,5 @@
-import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:get_it/get_it.dart';
-import 'package:rick_and_morty/core/network/network_client.dart';
-import 'package:rick_and_morty/feature/characters/data/api/character_api.dart';
-import 'package:rick_and_morty/feature/characters/data/repository/character_repository_impl.dart';
-import 'package:rick_and_morty/feature/characters/domain/usecases/get_chactacters_use_case.dart';
 
 import 'injector.config.dart';
 
@@ -16,17 +11,5 @@ final getIt = GetIt.instance;
   asExtension: true, // default
 )
 void configureDependencies() {
-  getIt.registerFactory<Dio>(() => Dio());
-  // getIt.registerFactory<Dio>(() => NetworkClient(Dio()).dio);
-
-  // //character
-  // getIt.registerLazySingleton<CharacterApi>(() => CharacterApi(dio: getIt()));
-  // getIt.registerLazySingleton<CharacterRepositoryImpl>(
-  //   () => CharacterRepositoryImpl(characterApi: getIt()),
-  // );
-  // getIt.registerLazySingleton<GetChactactersUseCase>(
-  //   () => GetChactactersUseCase(getIt()),
-  // );
-
   getIt.init();
 }
