02ff3a1d541a0ef207128b440cc3b0442d4af99f
Jacoai <grotesquevodevil@gmail.com>
2025-05-25 17:49:31 +0300

    refactor character view

--- lib/core/theme/src/text_style.dart
+++ lib/core/theme/src/text_style.dart
@@ -3,5 +3,5 @@ part of '../theme.dart';
 abstract class AppTextStyles {
   static const h1 = TextStyle(fontSize: 28);
   static const h5 = TextStyle(fontSize: 24);
-  static const baseText = TextStyle(fontSize: 16);
+  static const baseText = TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
 }
--- lib/feature/characters/presentation/widgets/character_card_view.dart
+++ lib/feature/characters/presentation/widgets/character_card_view.dart
@@ -16,8 +16,11 @@ class CharacterCardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
     return DecoratedBox(
       decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
         border: Border.all(
           color:
               Theme.of(context).extension<ThemeColors>()?.accentColor ??
@@ -25,63 +28,168 @@ class CharacterCardView extends StatelessWidget {
           width: 3,
         ),
       ),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+        child: IntrinsicHeight(
+          child: Row(
+            spacing: 12,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Expanded(
-                child: Text(
-                  character.name,
-                  style: AppTextStyles.h5,
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.clip,
-                ),
+              Column(
+                spacing: 8,
+                children: [
+                  SizedBox(
+                    width: width * 0.5,
+                    child: Text(
+                      character.name,
+                      style: AppTextStyles.h5,
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.clip,
+                      softWrap: true,
+                    ),
+                  ),
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                        color: AppColors.green,
+                        width: 3,
+                        strokeAlign: BorderSide.strokeAlignOutside,
+                      ),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.network(character.image, width: width * 0.5),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                ],
               ),
-              IconButton(
-                onPressed: () {
-                  if (character.isFavorite) {
-                    removeFromFavorite(character.id);
-                  } else {
-                    if (addToFavorite != null) addToFavorite!(character.id);
-                  }
-                },
-                icon:
-                    character.isFavorite
-                        ? Icon(
-                          Icons.star,
-                          color:
-                              Theme.of(
-                                context,
-                              ).extension<ThemeColors>()?.accentColor,
-                        )
-                        : Icon(
-                          Icons.star_border,
-                          color:
-                              Theme.of(
-                                context,
-                              ).extension<ThemeColors>()?.accentColor,
-                        ),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //spacing: 12,
+                  children: [
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        onPressed: () {
+                          if (character.isFavorite) {
+                            removeFromFavorite(character.id);
+                          } else {
+                            if (addToFavorite != null) {
+                              addToFavorite!(character.id);
+                            }
+                          }
+                        },
+                        icon:
+                            character.isFavorite
+                                ? Icon(
+                                  Icons.star,
+                                  color:
+                                      Theme.of(
+                                        context,
+                                      ).extension<ThemeColors>()?.accentColor,
+                                )
+                                : Icon(
+                                  Icons.star_border,
+                                  color:
+                                      Theme.of(
+                                        context,
+                                      ).extension<ThemeColors>()?.accentColor,
+                                ),
+                      ),
+                    ),
+                    Text(
+                      "Пол: ${character.gender}",
+                      style: AppTextStyles.baseText,
+                    ),
+                    Text(
+                      "Статус: ${character.status}",
+                      style: AppTextStyles.baseText,
+                    ),
+                    Text(
+                      "Вид: ${character.species}",
+                      style: AppTextStyles.baseText,
+                    ),
+                    const SizedBox(height: 8),
+                  ],
+                ),
               ),
             ],
           ),
-          DecoratedBox(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: AppColors.green,
-                width: 3,
-                strokeAlign: BorderSide.strokeAlignOutside,
-              ),
-            ),
-            child: Image.network(character.image),
-          ),
-          const SizedBox(height: 10),
-          Text("Пол: ${character.gender}", style: AppTextStyles.baseText),
-          Text("Статус: ${character.status}", style: AppTextStyles.baseText),
-          Text("Вид: ${character.species}", style: AppTextStyles.baseText),
-          Text("Пол: ${character.gender}", style: AppTextStyles.baseText),
-          const SizedBox(height: 10),
-        ],
+        ),
+        // Column(
+        //   spacing: 8,
+        //   children: [
+        //     Row(
+        //       mainAxisAlignment: MainAxisAlignment.center,
+        //       children: [
+        //         Expanded(
+        //           child: Text(
+        //             character.name,
+        //             style: AppTextStyles.h5,
+        //             textAlign: TextAlign.center,
+        //             overflow: TextOverflow.clip,
+        //           ),
+        //         ),
+        //         IconButton(
+        //           onPressed: () {
+        //             if (character.isFavorite) {
+        //               removeFromFavorite(character.id);
+        //             } else {
+        //               if (addToFavorite != null) addToFavorite!(character.id);
+        //             }
+        //           },
+        //           icon:
+        //               character.isFavorite
+        //                   ? Icon(
+        //                     Icons.star,
+        //                     color:
+        //                         Theme.of(
+        //                           context,
+        //                         ).extension<ThemeColors>()?.accentColor,
+        //                   )
+        //                   : Icon(
+        //                     Icons.star_border,
+        //                     color:
+        //                         Theme.of(
+        //                           context,
+        //                         ).extension<ThemeColors>()?.accentColor,
+        //                   ),
+        //         ),
+        //       ],
+        //     ),
+        //     Row(
+        //       children: [
+        //
+        //         Column(
+        //           children: [
+        //             Text(
+        //               "Пол: ${character.gender}",
+        //               style: AppTextStyles.baseText,
+        //             ),
+        //             Text(
+        //               "Статус: ${character.status}",
+        //               style: AppTextStyles.baseText,
+        //             ),
+        //             Text(
+        //               "Вид: ${character.species}",
+        //               style: AppTextStyles.baseText,
+        //             ),
+        //             Text(
+        //               "Пол: ${character.gender}",
+        //               style: AppTextStyles.baseText,
+        //             ),
+        //           ],
+        //         ),
+        //       ],
+        //     ),
+        //     const SizedBox(height: 8),
+        //   ],
+        // ),
       ),
     );
   }
